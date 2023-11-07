1c9f01ae086d9c0559ad5bf5b65b8e9c7ded33ea
UjjwalZambad <119690300+UjjwalZambad@users.noreply.github.com>
2023-11-07 20:51:20 +0530

    Update 01_knapsack.cpp

--- 01_knapsack.cpp
+++ 01_knapsack.cpp
@@ -1,41 +1,44 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int knapsack(int capacity, vector<int> &weights, vector<int> &values, int n)
-{
-    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int w = 0; w <= capacity; ++w)
-        {
-            if (weights[i - 1] <= w)
-            {
+const int MAX_N = 100; // Maximum number of items
+const int MAX_CAPACITY = 1000; // Maximum knapsack capacity
+
+int knapsack(int capacity, int weights[], int values[], int n) {
+    int dp[MAX_N + 1][MAX_CAPACITY + 1] = {0};
+
+    for (int i = 1; i <= n; ++i) {
+        for (int w = 0; w <= capacity; ++w) {
+            if (weights[i - 1] <= w) {
                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
-            }
-            else
-            {
+            } else {
                 dp[i][w] = dp[i - 1][w];
             }
         }
     }
+
     return dp[n][capacity];
 }
 
-int main()
-{
+int main() {
     int capacity;
-    cout << "Enter the capacity of the Knapsack" << endl;
+    cout << "Enter the capacity of the Knapsack: " << endl;
     cin >> capacity;
+    
     int n;
-    cout << "Enter the number of items" << endl;
+    cout << "Enter the number of items: " << endl;
     cin >> n;
-    vector<int> weights(n), values(n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter weight and value for item" << i + 1 << ":";
+
+    int weights[MAX_N];
+    int values[MAX_N];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter weight and value for item " << i + 1 << ": ";
         cin >> weights[i] >> values[i];
     }
-    double result = knapsack(capacity, weights, values, n);
+
+    int result = knapsack(capacity, weights, values, n);
     cout << "Maximum value obtained is " << result << endl;
+
     return 0;
 }
