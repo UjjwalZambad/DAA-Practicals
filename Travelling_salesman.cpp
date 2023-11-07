202df09413062c984c5fe8fe8d896437702297b3
UjjwalZambad <119690300+UjjwalZambad@users.noreply.github.com>
2023-11-07 23:00:32 +0530

    Update Travelling_salesman.cpp

--- Travelling_salesman.cpp
+++ Travelling_salesman.cpp
@@ -1,63 +1,49 @@
-#include <iostream>
-#include <climits>
-#include <algorithm> // Added for 'copy' function
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_N = 10;
 const int INF = INT_MAX;
-int n;
-int graph[MAX_N][MAX_N];
-bool visited[MAX_N];
-int lowerBound = INT_MAX; // Initialize to a high value
 
-void inputGraph() {
-    cout << "Enter the number of cities: ";
-    cin >> n;
-    cout << "Enter the adjacency matrix:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> graph[i][j];
-            if (i != j && graph[i][j] == 0) {
-                graph[i][j] = INF;
-            }
-        }
+int tsp(int graph[][10], bool visited[], int currentPos, int n, int count, int cost, int& ans) {
+    if (count == n && graph[currentPos][0]) {
+        ans = min(ans, cost + graph[currentPos][0]);
+        return ans;
     }
-}
 
-void LCBB(int currPos, int cost, int level, int path[MAX_N]) {
-    if (level == n) {
-        if (graph[currPos][0] != INF) {
-            int currentCost = cost + graph[currPos][0];
-            if (currentCost < lowerBound) {
-                lowerBound = currentCost;
-                copy(path, path + n, path + 1); // Fix path copy
-                path[0] = 0; // Start and end at city 0
-            }
-        }
-        return;
-    }
-    for (int i = 0; i < n; i++) {
-        if (!visited[i] && graph[currPos][i] != INF) {
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i] && graph[currentPos][i]) {
             visited[i] = true;
-            path[level] = i;
-            LCBB(i, cost + graph[currPos][i], level + 1, path);
+            tsp(graph, visited, i, n, count + 1, cost + graph[currentPos][i], ans);
             visited[i] = false;
         }
     }
+    return ans;
+}
+
+int branchAndBoundTSP(int graph[][10], int n) {
+    bool visited[10] = { false };
+    visited[0] = true; // Starting from the 0th vertex
+
+    int ans = INF;
+    ans = tsp(graph, visited, 0, n, 1, 0, ans);
+    return ans;
 }
 
 int main() {
-    inputGraph();
-    int path[MAX_N];
-    visited[0] = true;
-    path[0] = 0;
-    LCBB(0, 0, 1, path);
+    int n;
+    cout << "Enter the number of cities: ";
+    cin >> n;
+    int graph[10][10];
 
-    cout << "Optimal Path: ";
-    for (int i = 0; i < n; i++) {
-        cout << path[i] << " ";
+    cout << "Enter the adjacency matrix for the graph: " << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> graph[i][j];
+        }
     }
-    cout << "0\n"; // Return to starting city
-    cout << "Cost: " << lowerBound << "\n";
+
+    int minCost = branchAndBoundTSP(graph, n);
+
+    cout << "Minimum cost for TSP: " << minCost << endl;
+
     return 0;
 }
