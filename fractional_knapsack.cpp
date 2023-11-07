ce80d5e855940fdf693e1648bb8e7ced2cccffe2
UjjwalZambad <119690300+UjjwalZambad@users.noreply.github.com>
2023-11-07 15:56:40 +0530

    Update fractional_knapsack.cpp

--- fractional_knapsack.cpp
+++ fractional_knapsack.cpp
@@ -1,28 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Item{
+struct Item {
     int weight;
     int value;
 };
 
-bool compare(Item i1, Item i2){
-    double ratio1 = double(i1.value)/i1.weight;
-    double ratio2 = double(i2.value)/i2.weight;
+bool compare(Item i1, Item i2) {
+    double ratio1 = double(i1.value) / i1.weight;
+    double ratio2 = double(i2.value) / i2.weight;
     return ratio1 > ratio2;
 }
 
-double fractionalKnapsack(int capacity, vector<Item>& items){
-    sort (items.begin(), items.end(), compare);
+double fractionalKnapsack(int capacity, Item items[], int n) {
+    sort(items, items + n, compare);
     double totalValue = 0.0;
-    for(Item& item : items){
-        if(item.weight <= capacity){
-            totalValue += item.value;
-            capacity -= item.weight;
-        }
-        else{
-        totalValue += (double)item.value * capacity / item.weight;
-        break;
+    
+    for (int i = 0; i < n; i++) {
+        if (items[i].weight <= capacity) {
+            totalValue += items[i].value;
+            capacity -= items[i].weight;
+        } else {
+            totalValue += double(items[i].value) * capacity / items[i].weight;
+            break;
         }
     }
     return totalValue;
@@ -30,17 +30,21 @@ double fractionalKnapsack(int capacity, vector<Item>& items){
 
 int main() {
     int capacity;
-    cout<<"Enter the capacity of the Knapsack"<<endl;
-    cin>>capacity;
+    cout << "Enter the capacity of the Knapsack: " << endl;
+    cin >> capacity;
+    
     int n;
-    cout<<"Enter the number of items"<<endl;
-    cin>>n;
-    vector<Item> items(n);
-    for(int i=0; i<n; i++){
-        cout<<"Enter weight and value for item"<<i+1<<":";
-        cin>>items[i].weight>>items[i].value;
+    cout << "Enter the number of items: " << endl;
+    cin >> n;
+    
+    Item items[n];
+    for (int i = 0; i < n; i++) {
+        cout << "Enter weight and value for item " << i + 1 << ": ";
+        cin >> items[i].weight >> items[i].value;
     }
-    double result = fractionalKnapsack(capacity,items);
-    cout<<"Maximum value obtained is "<<result<<endl;
+    
+    double result = fractionalKnapsack(capacity, items, n);
+    cout << "Maximum value obtained is " << result << endl;
+    
     return 0;
 }
